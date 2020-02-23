92c76585eb8e073af37b27d4baaab2a9deadd7a8
mi81ma <mi81ma@gmail.com>
2020-02-23 14:37:45 +0800

    Change firstView from ScannerViewController to ViewController

--- SourceDLT_App/AppDelegate.swift
+++ SourceDLT_App/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // ViewControllerのインスタンス
-        let firstView = ScannerViewController()
+        let firstView = ViewController()
 
         //        // Navigation Controller のインスタンス
         //        let navigationController = UINavigationController(rootViewController: firstView)
