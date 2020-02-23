c588a5f9242f89aa4d9f4518ada0336adfa5dc8c
mi81ma <mi81ma@gmail.com>
2020-02-23 14:24:36 +0800

    Add ScannerViewController as first View

--- SourceDLT_App/AppDelegate.swift
+++ SourceDLT_App/AppDelegate.swift
@@ -15,7 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // ViewControllerのインスタンス
+        let firstView = ScannerViewController()
+
+        //        // Navigation Controller のインスタンス
+        //        let navigationController = UINavigationController(rootViewController: firstView)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        // set NavigationController to root View
+        self.window?.rootViewController = firstView
+
+        window?.backgroundColor = .white
+
+
+        window?.makeKeyAndVisible()
         return true
     }
 
